1f4205dea012f1caa1558d66879e80ae32016d16
v1v3k6 <vivekbiswas@skava.com>
2019-09-04 17:57:42 +0530

    Misc changes
    * Add react-image-zoom package
    * Split large functions into chunks for better understanding
    * Resize product images to work with react-image-zoom component

--- src/App.js
+++ src/App.js
@@ -31,39 +31,62 @@ class App extends React.Component
     }
   }
 
-  AddToCart()
+  AddToCart(val)
   {
-    this.setState({
-      qty: this.state.qty + 1
+    if(val != -1 || this.state.qty>0)
+    {
+      this.setState({
+      qty: this.state.qty + val
     });
+    }
+    else
+      alert("Cart is empty! Please add before removing...");
+  }
+
+  setHeader()
+  {
+    return(
+      
+      <div id="hinner">
+        <div id="hleft">Shopping Site Dummy</div>
+        <div id="hright">Cart: {this.state.qty}</div>
+      </div>
+    );
+  }
+
+  setFooter()
+  {
+    return(
+      <strong>Footer</strong>
+    );
+  }
+
+  loadContent()
+  {
+    return(
+    <div id="content">
+      {Object.keys(product_data.Brand).map(manufacturers => (
+        <div id="product">
+          <ul>
+            <h1>{manufacturers}:</h1>
+              {Object.keys(product_data.Brand[manufacturers]).map(specs => (
+                <div id={specs}>{this.parseData(product_data,manufacturers, specs)}</div>
+              ))}
+          </ul>
+          <div><img height="600px" width="550px" id="Pics" src={product_data.Brand[manufacturers].Pics[0]} alt={manufacturers} /><img height="500px" width="450px" id="Pics" src={product_data.Brand[manufacturers].Pics[1]} alt={manufacturers} /></div><br />
+          <center><button onClick={(param) => this.AddToCart(1)}>Add to Cart</button><button onClick={(param) => this.AddToCart(-1)}>Remove from Cart</button></center><br />
+        </div>
+        ))}
+    </div>
+    );
   }
 
   render() {
     return (
       <div className="App">
-        <header id="hroot">
-            <div id="hinner">
-                <div id="hleft">Shopping Site Dummy</div>
-                <div id="hright">Cart: {this.state.qty}</div>
-            </div>
-        </header>
-        <div id="croot">
-          <div id="content">
-            {Object.keys(product_data.Brand).map(manufacturers => (
-              <div id="product">
-              <ul>
-                <h1>{manufacturers}:</h1>
-                {Object.keys(product_data.Brand[manufacturers]).map(specs => (
-                  <div id={specs}>{this.parseData(product_data,manufacturers, specs)}</div>
-                ))}
-              </ul>
-              <div><img height="500px" width="450px" id="Pics" src={product_data.Brand[manufacturers].Pics[0]} alt={manufacturers} /><img height="500px" width="450px" id="Pics" src={product_data.Brand[manufacturers].Pics[1]} alt={manufacturers} /></div><br />
-              <center><button onClick={this.AddToCart}>Add to Cart</button></center><br />
-              </div>
-            ))}
-          </div>
-        </div>
-        <footer id="froot">Footer</footer>
+        <header id="hroot">{this.setHeader()}</header>
+        <div id="croot">{this.loadContent()}</div>
+        <footer id="froot">{this.setFooter()}</footer>
       </div>
     );
   }
